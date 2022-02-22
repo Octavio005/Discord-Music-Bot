b6ad278af5580e76d312a612bfcea001d6c0ffdf
Octavio <octaviogcensori@gmail.com>
2022-02-22 18:24:04 -0300

    Downgraded node.js version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 const yts = require("yt-search");
 
+
 const client = new Discord.Client();
 
 const queue = new Map();
